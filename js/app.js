0fc47add73c2d01723dbfa1f2eb94b45415f0020
Ravi Kumar Seth <ravi.kseth.che12@iitbhu.ac.in>
2017-09-06 16:58:46 +0530

    Udacity Submit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,181 @@
 /*
- * Create a list that holds all of your cards
+ * A list that holds all of your cards
  */
+let currentCardIndex = -1;
+let cards = ["diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf",
+  "bicycle", "bomb"
+];
 
+// Double each type of the initial cards
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+cards = cards.concat(cards);
+
+// Few variable declarations
+
+let moves = 0;
+let matches = 0;
+let time = 0;
+let interval = null;
+
+// Initial Game Setup
+
+function setupGame() {
+
+  // Shuffle all the cards before showing them
+  cards = shuffle(cards);
+
+  // Show game window if previously hidden by Score Card
+  showGameWindow();
+
+  // Start the timer clock when game starts
+  interval = setInterval(function() {
+    time++;
+    $('.timer').text(time);
+  }, 1000);
+
+  // Reset the deck
+  $(".deck").html('');
+
+  // Add Random Cards to the Deck
+  cards.map((card) => {
+    $('.deck').append("<li class='card'><i class='fa fa-" +
+      card + "'></i></li>");
+  });
+
+  // Card click listner
+  $(".deck li").on('click', function() {
+    let index = $(this).index();
+    $(".deck li").eq(index).addClass('open show');
+    if (currentCardIndex !== index) {
+      // When the card is first card
+      if (currentCardIndex === -1) {
+        openNewCard(index);
+        // When the second card matches first
+      } else if (cards[currentCardIndex] === cards[index]) {
+        cardMatched(index);
+        // When the second card doesn't match first
+      } else {
+        cardUnMatched(index);
+      }
+      moves++;
+      setRatings(moves);
+      // End game condition
+      if (matches === 8) {
+        showScoreCard();
+        if (interval) {
+          clearInterval(interval);
+        }
+      }
+    }
+    // Udate number of moves on Each card match or unmatch
+    $('.moves').text(Math.floor(moves / 2));
+  });
+}
+
+// Changes star rating based on number of moves
+
+function setRatings(moves) {
+  $('.stars').html('');
+  if (moves > 34) {
+    $('.stars').html('<li><i class="fa fa-star"></i></li><li>');
+  } else if (moves > 26 && moves <= 34) {
+    $('.stars').html('<li><i class="fa fa-star"></i></li>' +
+      '<li><i class="fa fa-star"></i></li>');
+  } else {
+    $('.stars').html('<li><i class="fa fa-star"></i></li>' +
+      '<li><i class="fa fa-star"></i></li>' +
+      '<li><i class="fa fa-star"></i></li>');
+  }
+}
+
+// This function is called when the card turned is the first card
+
+function openNewCard(index) {
+  currentCardIndex = index;
+}
+
+// Contols what happens if 2nd card matches 1st card
+
+function cardMatched(index) {
+  matches++;
+  setTimeout(() => {
+    $(".deck li").eq(currentCardIndex).removeClass('show');
+
+    $(".deck li").eq(currentCardIndex).addClass('match');
+    $(".deck li").eq(index).addClass('match');
+
+    $(".deck li").eq(currentCardIndex).off('click');
+    $(".deck li").eq(index).off('click');
+
+    currentCardIndex = -1;
+  }, 200);
+
+}
+
+// Contols what happens when 2nd card doesn't match 1st
+
+function cardUnMatched(index) {
+  setTimeout(() => {
+
+    $(".deck li").eq(currentCardIndex).removeClass('show');
+    $(".deck li").eq(index).removeClass('show');
+
+    currentCardIndex = -1;
+  }, 200);
+}
+
+// Shows game when game is started, hides score card
+
+function showGameWindow() {
+  $('.deck').show();
+  $('.score-panel').show();
+  $('.final-score').hide();
+}
+
+// Shows score card when game ends, hides game window
+
+function showScoreCard() {
+  $('.deck').hide();
+  $('.score-panel').hide();
+  $('.final-score').show();
+}
+
+// Resets the game
+
+function reset() {
+  time = 0;
+  moves = 0;
+  matches = 0;
+  $('.moves').text(0);
+  $('.timer').text(0);
+  if (interval) {
+    clearInterval(interval);
+  }
+  showGameWindow();
+  setupGame();
+  setRatings(moves);
+}
+
+// Loads game when document is ready
+
+$(function() {
+  setupGame();
+  $('.restart').click(reset);
+});
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
